de8dad607c8424f93b0f4eef9e27e9c0177ae1ce
pratik1243 <thakurpratik226@gmail.com>
2024-06-02 17:35:47 +0530

    sidebar menu theme btn changes

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -18,7 +18,7 @@ const Sidebar = ({
         >
           <h1>P</h1>
 
-          <div className="menu-close-btn" onClick={closeSidebar}>
+          <button className="menu-close-btn" onClick={closeSidebar}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="31"
@@ -29,7 +29,7 @@ const Sidebar = ({
             >
               <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
             </svg>
-          </div>
+          </button>
         </div>
         <ul
           className={`responsive-menu-list ${
@@ -96,6 +96,7 @@ const Sidebar = ({
           className="theme-btn mobile-theme-btn"
           onClick={() => {
             setTheme(!theme);
+            closeSidebar();
           }}
         >
           {theme ? (
